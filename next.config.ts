d7079c48ed7ecd1fcb30e7f1c7fef99057e68813
Francesco Miragliuolo <francescomiragliuolo@MACBOOKPRO-7CF1.station>
2026-05-19 03:41:49 +0200

    fix: canvas alias per pdfjs

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_BUILD_ID: new Date().getTime().toString(),
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias.canvas = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
